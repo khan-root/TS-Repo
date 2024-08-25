076352a3a137bb809b1abff1bc8f4e63b080cc49
khan_root <sarmadf49@gmail.com>
2024-08-25 20:28:55 +0500

    alias

---
+++ first.ts
@@ -0,0 +1,16 @@
+const num1: number = 2
+console.log(num1)
+
+type User = {
+    name: String,
+    age: number
+    address?:String
+} 
+
+
+const user : User={
+    name: 'Ali',
+    age:12
+}
+
+console.log(user)
